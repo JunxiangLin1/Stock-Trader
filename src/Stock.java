d36d88b73914a62db7cc230c895eb407221b09e8
Eric Huang <ejwhuang@gmail.com>
2024-06-04 20:34:03 -0400

    started the Stock implementation

--- src/Stock.java
+++ src/Stock.java
@@ -1,2 +1,11 @@
+import java.util.List;
+
 public interface Stock {
+
+  boolean increase (Date start, Date end);
+
+  double xDayMovingAverage(Date date, int days);
+
+  List<Date> xDayCrossovers(Date start, Date end, int day);
+
 }
