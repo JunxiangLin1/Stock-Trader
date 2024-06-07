40000dff86a6d62462f89b6bbd00de1f0b963130
Junxiang Lin <lin.junx@northeastern.edu>
2024-06-07 19:25:27 -0400

    Created Mock Stock

---
+++ src/models/impl/SimpleStock.java
@@ -0,0 +1,78 @@
+package models.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import models.Date;
+import models.Stock;
+
+/**
+ * Implements the {@link Stock} interface. This class represents a stock with
+ * its ticker symbol, number of shares, and daily price data.
+ */
+public class SimpleStock implements Stock {
+  private final String ticker;
+  private final Map<Date, List<Double>> data;
+  private int shares;
+
+  /**
+   * Constructs a {@code StockImpl} object with the specified ticker and number of shares.
+   *
+   * @param ticker the ticker symbol of the stock
+   * @param shares the number of shares
+   */
+  public SimpleStock(String ticker, int shares) {
+    this.ticker = ticker;
+    this.data = new HashMap<Date, List<Double>>();
+    this.shares = shares;
+  }
+
+
+  @Override
+  public void addData(Date date, double open, double high, double low, double close,
+                      double volume) {
+    this.data.put(date, new ArrayList<Double>(List.of(open, high, low, close, volume)));
+  }
+
+  @Override
+  public HashMap<Date, List<Double>> getData() {
+    return new HashMap<>(this.data);
+  }
+
+  @Override
+  public int getShares() {
+    return this.shares;
+  }
+
+  @Override
+  public String getTicker() {
+    return this.ticker;
+  }
+
+  @Override
+  public double getOpen(Date day) {
+    return this.data.get(day).get(0);
+  }
+
+  @Override
+  public double getHigh(Date day) {
+    return this.data.get(day).get(1);
+  }
+
+  @Override
+  public double getLow(Date day) {
+    return this.data.get(day).get(2);
+  }
+
+  @Override
+  public double getClose(Date day) {
+    return this.data.get(day).get(3);
+  }
+
+  @Override
+  public double getVolume(Date day) {
+    return this.data.get(day).get(3);
+  }
+}
--- src/models/impl/StockImpl.java
+++ src/models/impl/StockImpl.java
@@ -1,82 +1,23 @@
 package models.impl;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import models.Date;
-import models.Stock;
 import models.stockFunctions.PopulateData;
 
 /**
- * Implementation of the {@link Stock} interface. This class represents a stock with
- * its ticker symbol, number of shares, and daily price data.
+ * Extends the {@link SimpleStock} class.
+ * This class represents a stock with its ticker symbol, number of shares, and daily price data.
+ * Represents a stock with data from Vantage API.
  */
-public class StockImpl implements Stock {
-
-  String ticker;
-
-  Map<Date, List<Double>> data;
-
-  int shares;
+public class StockImpl extends SimpleStock {
 
   /**
    * Constructs a {@code StockImpl} object with the specified ticker and number of shares.
+   * Then populates the stock using data from Alpha Vantage API
    *
    * @param ticker the ticker symbol of the stock
    * @param shares the number of shares
    */
   public StockImpl(String ticker, int shares) {
-    this.ticker = ticker;
-    this.data = new HashMap<Date, List<Double>>();
-    this.shares = shares;
+    super(ticker, shares);
     new PopulateData().apply(this);
-  }
-
-  @Override
-  public void addData(Date date, double open, double high, double low, double close,
-                      double volume) {
-    this.data.put(date, new ArrayList<Double>(List.of(open, high, low, close, volume)));
-  }
-
-  @Override
-  public HashMap<Date, List<Double>> getData() {
-    return new HashMap<>(this.data);
-  }
-
-  @Override
-  public int getShares() {
-    return this.shares;
-  }
-
-  @Override
-  public String getTicker() {
-    return this.ticker;
-  }
-
-  @Override
-  public double getOpen(Date day) {
-    return this.data.get(day).get(0);
-  }
-
-  @Override
-  public double getHigh(Date day) {
-    return this.data.get(day).get(1);
-  }
-
-  @Override
-  public double getLow(Date day) {
-    return this.data.get(day).get(2);
-  }
-
-  @Override
-  public double getClose(Date day) {
-    return this.data.get(day).get(3);
-  }
-
-  @Override
-  public double getVolume(Date day) {
-    return this.data.get(day).get(3);
   }
 }
